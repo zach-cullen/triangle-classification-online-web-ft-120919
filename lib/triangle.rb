d4866481500784608620d71e04c8c0c5a1539eec
Zach Cullen <zacullen@gmail.com>
2020-01-08 19:33:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_accessor :kind
+  attr_reader :sides
+  
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3].sort!
+    if self.is_valid?
+      self.find_kind
+    else
+      puts "I'm not valid"
+    end
+  end
+  
+  def is_valid?
+    if @sides.include?(0)
+      false
+    elsif @sides[0] + @sides[1] < @sides[2]
+      false
+    else
+      true
+    end
+  end
+  
+  def find_kind
+    if @sides[0] == @sides[1] && @sides[1] == @sides[2]
+      @kind = :equilateral
+    elsif @side1 == @side2 || @side2 == @side3
+      @kind = :isosceles
+    else
+      @kind = :scalene
+    end
+  end
 end
